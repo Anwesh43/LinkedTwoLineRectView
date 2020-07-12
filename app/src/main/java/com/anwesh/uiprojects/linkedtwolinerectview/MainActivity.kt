33b33a64b13e0c3931918c61afb1db242aa7c289
Anwesh43 <anweshthecool0@gmail.com>
2020-07-13 04:45:12 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtwolinerectview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtwolinerectview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedtwolinerectview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.twolinerectview.TwoLineRectView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TwoLineRectView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
