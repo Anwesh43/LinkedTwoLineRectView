0007d21e2f1f5d949beda7d1c7c9646ae79c970f
Anwesh43 <anweshthecool0@gmail.com>
2020-07-13 04:09:09 +0530

    adding constants and libraries

--- twolinerectview/src/main/java/com/anwesh/uiprojects/twolinerectview/TwoLineRectView.kt
+++ twolinerectview/src/main/java/com/anwesh/uiprojects/twolinerectview/TwoLineRectView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.twolinerectview
  * Created by anweshmishra on 13/07/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<String> = arrayOf("#4CAF50", "#F44336", "#03A9F4", "#3F51B5", "#FF9800")
+val parts : Int = 3
+val scGap : Float = 0.02f / 3
+val widthFactor : Float = 3.4f
+val strokeFactor : Int = 90
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
