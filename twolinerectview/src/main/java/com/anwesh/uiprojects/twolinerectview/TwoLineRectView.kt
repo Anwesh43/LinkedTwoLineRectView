278d0874293be5f2bf90fab4bbf130f99a003165
Anwesh43 <anweshthecool0@gmail.com>
2020-07-13 04:21:18 +0530

    adding Canvas extension method

--- twolinerectview/src/main/java/com/anwesh/uiprojects/twolinerectview/TwoLineRectView.kt
+++ twolinerectview/src/main/java/com/anwesh/uiprojects/twolinerectview/TwoLineRectView.kt
@@ -25,3 +25,25 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawTwoLineRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val wBar : Float = w / widthFactor
+    for (j in 0..1) {
+        val sfj : Float = sf.divideScale(j, 3)
+        drawLine(-wBar / 2, 0f, wBar / 2, h * sfj, paint)
+    }
+    drawRect(RectF(-wBar, 0f, wBar, h * sf.divideScale(2, parts)), paint)
+}
+
+fun Canvas.drawTLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, 0f)
+    drawTwoLineRect(scale, w, h, paint)
+    restore()
+}
